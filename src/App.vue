225c30126d08e925c2bb4f3a8a82e023f9ba7192
lzy <896002844@qq.com>
2025-11-28 16:04:07 +0800

    删去默认hello代码

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script lang="ts" setup>
 import { onMounted, ref } from 'vue';
-import Hello from './Hello/index.vue'
 import Read from './Read/index.vue'
 import Write from './Write/index.vue'
 import Clipboard from './Clipboard/index.vue'
@@ -27,9 +26,6 @@ onMounted(() => {
 <template>
   <template v-if="route === 'clipboard' || route === ''">
     <Clipboard></Clipboard>
-  </template>
-  <template v-if="route === 'hello'">
-    <Hello :enterAction="enterAction"></Hello>
   </template>
   <template v-if="route === 'read'">
     <Read :enterAction="enterAction"></Read>
